01c6b5fd6415a985b897b33f81496890ef425cdc
Radoslaw Napora <Callenowy@users.noreply.github.com>
2024-01-31 21:35:23 +0100

    feat(next.config): enable reactStrictMode
    - disable poweredByHeader,
    - set output to standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  output: 'standalone',
+};
 
 export default nextConfig;
